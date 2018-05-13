6689e1f4455ff923765d1e8e285b2b6ebe7588c2
avivit100 <avivit100@yahoo.com>
2018-05-13 20:16:31 +0300

    add aaa

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -3,5 +3,6 @@ public class MainClass {
     public static void main (String args[]){
 	//My comant after adding this file
         System.out.println("Start");
+	System.out.println("add aaa");
     }
 }
